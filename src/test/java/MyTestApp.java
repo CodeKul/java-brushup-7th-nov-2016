cd1e8d403db3f49e202d66f21f15a42d836b852d
aniruddha <aniruddha.kudalkar@gmail.com>
2016-11-07 19:21:55 +0530

    testing added

---
+++ src/test/java/MyTestApp.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by aniruddha on 7/11/16.
+ */
+public class MyTestApp {
+
+    @Test
+    public void testOne(){
+        Assert.assertEquals(12,12);
+        System.out.println("yes its");
+    }
+}
